06dfab5fa1fc122a8c3858d693ebf375d389093b
Daniel Holmlund <agnathan@gmail.com>
2024-04-20 21:39:19 -0700

    the whole app

---
+++ src/pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from "next/document"
+
+export default function Docuemnt() {
+    return (
+        <Html data-theme="winter">
+            <Head />
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
